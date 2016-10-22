265e759636c74bef5f90bcf2d68c1a8008879b57
ICalvinG <calvinieong1118@gmail.com>
2016-10-22 14:11:32 -0400

    Search Giphy for gif and successfully grab url

--- app/controllers/giphies_controller.rb
+++ app/controllers/giphies_controller.rb
@@ -2,6 +2,13 @@ class GiphiesController < ApplicationController
 
 	def index
 		@giphies = Giphy.all
+		if params["gif"].nil? == false
+			gif = params["gif"]
+			searched_gif = Giphy.new(title: gif)
+			searched_gif.save
+			searched_gif.post
+			Giphy.find(1).destroy
+		end
 	end
 
 end
