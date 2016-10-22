d2478fe5a8e5cf6365ab795cc0c1596ef0269efa
ICalvinG <calvinieong1118@gmail.com>
2016-10-22 15:09:40 -0400

    Fix case where new Giphy class id was not 1

--- app/controllers/giphies_controller.rb
+++ app/controllers/giphies_controller.rb
@@ -4,10 +4,10 @@ class GiphiesController < ApplicationController
 		@giphies = Giphy.all
 		if params["gif"].nil? == false
 			gif = params["gif"]
-			searched_gif = Giphy.new(title: gif)
+			searched_gif = Giphy.new(id: 1, title: gif)
 			searched_gif.save
 			searched_gif.post
-			Giphy.find(1).destroy
+			Giphy.destroy_all
 		end
 	end
 
