85e791b0e1df907159b5215c1b5072f5857cbea4
ICalvinG <calvinieong1118@gmail.com>
2016-12-06 16:17:43 -0500

    Create giphy_api_key

--- app/models/giphy.rb
+++ app/models/giphy.rb
@@ -2,9 +2,8 @@ class Giphy < ApplicationRecord
 	include HTTParty
 	base_uri "http://api.giphy.com/v1/gifs/random?"
 
-	giphy_key = Rails.application.secrets.giphy_api_key
-
 	def post
+		giphy_key = Rails.application.secrets.giphy_api_key
 		tag = Giphy.find(1).title.downcase.gsub(/[ ]/, "+")
 		data = self.class.get("api_key=#{giphy_key}&tag=#{tag}")
 		json_file = JSON.parse(data.body)
