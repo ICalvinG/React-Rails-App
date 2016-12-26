d90a200a51f1c742e60492df118c70ddac24d233
ICalvinG <calvinieong1118@gmail.com>
2016-12-26 14:45:47 -0500

    Add faceplam gif to none view/Refactor

--- app/controllers/giphies_controller.rb
+++ app/controllers/giphies_controller.rb
@@ -11,15 +11,13 @@ class GiphiesController < ApplicationController
 				@searched_gif.save
 				if @searched_gif.save
 					@searched_gif.post
-					@gif_result = Giphy.find(1)
-					if !@gif_result.url.nil?
+					if !@searched_gif.url.nil?
 						render 'success'
 					else
-						@fail_gif = Giphy.new(id: 2, title: "sorry")
+						@fail_gif = Giphy.new(id: 2, title: "facepalm")
 						@fail_gif.save
 						if @fail_gif.save
 							@fail_gif.post
-							@sorry_gif = Giphy.find(2)
 						end
 						render 'none'
 					end
