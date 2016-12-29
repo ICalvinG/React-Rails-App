60d86e0d3582409bbd08aa9e1cda802703cbe24b
ICalvinG <calvinieong1118@gmail.com>
2016-12-29 17:41:35 -0500

    Create trend method to gather and create trending Giphy objects

--- app/models/giphy.rb
+++ app/models/giphy.rb
@@ -1,17 +1,31 @@
 class Giphy < ApplicationRecord
 	include HTTParty
-	base_uri "http://api.giphy.com/v1/gifs/random?"
+	base_uri "http://api.giphy.com/v1/gifs"
 
 	def post
 		giphy_key = Rails.application.secrets.giphy_api_key
 		tag = self.title.downcase.gsub(/[ ]/, "+")
-		data = self.class.get("api_key=#{giphy_key}&tag=#{tag}")
+		data = self.class.get("/random?api_key=#{giphy_key}&tag=#{tag}")
 		json_file = JSON.parse(data.body)
 		if data['data'].any?
 			self.update_attributes(
 				:url => data['data']['fixed_height_downsampled_url'],
 				:height => data['data']['fixed_height_downsampled_height'],
 				:width => data['data']['fixed_height_downsampled_width']
+				)
+			self.save
+		end
+	end
+
+	def trend
+		giphy_key = Rails.application.secrets.giphy_api_key
+		data = self.class.get("/trending?api_key=#{giphy_key}")
+		json_file = JSON.parse(data.body)
+		if data['data'].any?
+			self.update_attributes(
+				:url => data["data"][self.id]["images"]["fixed_width"]["url"],
+				:height => data["data"][self.id]["images"]["fixed_width"]["height"],
+				:width => 200
 				)
 			self.save
 		end
